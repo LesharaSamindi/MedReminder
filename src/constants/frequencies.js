71866919deb724f3a51edc2136de2caf07434de7
LesharaSamindi <lesharasamindi@gmail.com>
2026-08-10 16:35:00 +0530

    Add medicine frequency selection options

---
+++ src/constants/frequencies.js
@@ -0,0 +1,20 @@
+export const FREQUENCIES = [
+  {
+    id: 'once_daily',
+    label: 'Once Daily',
+  },
+  {
+    id: 'twice_daily',
+    label: 'Twice Daily',
+  },
+  {
+    id: 'every_6_hours',
+    label: 'Every 6 Hours',
+  },
+  {
+    id: 'custom',
+    label: 'Custom',
+  },
+];
+
+export const MAX_CUSTOM_TIMES = 5;
