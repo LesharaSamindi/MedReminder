0c104e7d43a05634a4201f3c97bfbd30f83cb07f
LesharaSamindi <lesharasamindi@gmail.com>
2026-08-11 11:45:00 +0530

    Add once daily twice daily and six hour schedules

--- src/utils/scheduleHelpers.js
+++ src/utils/scheduleHelpers.js
@@ -1,5 +1,32 @@
 import { toHourMinute } from './dateTimeHelpers';
 
+export function generateOnceDaily(startDate) {
+  return [startDate];
+}
+
+export function generateTwiceDaily(firstTime, secondTime) {
+  return [toHourMinute(firstTime), toHourMinute(secondTime)];
+}
+
+export function generateEverySixHours(startDate) {
+  const times = [];
+  for (let i = 0; i < 4; i++) {
+    const next = new Date(startDate);
+    next.setHours(startDate.getHours() + i * 6, startDate.getMinutes(), 0, 0);
+    times.push(toHourMinute(next));
+  }
+  return times;
+}
+
 export function buildSchedule({ frequency, startDate, firstTime, secondTime, customTimes }) {
-  return [toHourMinute(startDate)];
+  switch (frequency) {
+    case 'once_daily':
+      return [toHourMinute(startDate)];
+    case 'twice_daily':
+      return generateTwiceDaily(firstTime, secondTime);
+    case 'every_6_hours':
+      return generateEverySixHours(startDate);
+    default:
+      return [toHourMinute(startDate)];
+  }
 }
